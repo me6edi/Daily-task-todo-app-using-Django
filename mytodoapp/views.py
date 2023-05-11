657f971034c9bdea6b26d50b504e0959a25eb14e
me6edi <exited17@gmail.com>
2023-05-11 22:35:29 +0600

    create models and catch the data into template using tamplate tag

--- mytodoapp/views.py
+++ mytodoapp/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Task
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    todos = Task.objects.all()
+    context = {
+        'todo': todos
+    }
+    return render(request, 'index.html',context)
 
 def about(request):
     return HttpResponse("<h1>About pge</h1>")
