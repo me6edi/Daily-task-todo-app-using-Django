dee39933d8926ab87145ca03f6e60d2681212be7
me6edi <exited17@gmail.com>
2023-05-12 21:49:27 +0600

    create a froms

--- mytodoapp/views.py
+++ mytodoapp/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
+
 # Create your views here.
 def index(request):
     todos = Task.objects.all()
+
+    if request.method == "POST":
+        forms = TaskForm(request.POST)
+        if forms.is_valid():
+            forms.save()
+            return redirect('/')
+    else:
+        forms = TaskForm()
+
     context = {
-        'todo': todos
+        'todo': todos,
+        'form' : forms,
     }
     return render(request, 'index.html',context)
 
